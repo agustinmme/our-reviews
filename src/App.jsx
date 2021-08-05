f9bc6218ae6773c47852b5b3ced3e32114798a9a
agustinmme <agustinmansilla240@gmail.com>
2021-08-05 19:26:10 -0300

    Feature : next,previous,random

---
+++ src/App.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import Review from "./Review.jsx";
+import data from "./data.js";
+import { Button, Flex, Stack, Heading } from "@chakra-ui/react";
+
+import "./App.css";
+
+function App() {
+  const [index, setIndex] = useState(0);
+  const checkData = (number) => {
+    if (number > data.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return data.length - 1;
+    }
+    return number;
+  };
+  const nextData = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return checkData(newIndex);
+    });
+  };
+  const prevData = () => {
+    setIndex((index) => {
+      let newIndex = index - 1;
+      return checkData(newIndex);
+    });
+  };
+  const randomData = () => {
+    let random = Math.floor(Math.random() * data.length);
+    if (random === index) {
+      random = index + 1;
+    }
+    setIndex(checkData(random));
+  };
+  return (
+    <div className="App">
+      <div className="App-header">
+        <Stack mb={5} align="center">
+          <Heading>Our Reviews</Heading>
+          <div className="underline" />
+        </Stack>
+        <Review index={index} data={data} />
+        <Flex direction={"row"} mt={5}>
+          <Button
+            colorScheme="blue"
+            mr={3}
+            onClick={() => {
+              prevData();
+            }}
+          >
+            Previous
+          </Button>
+          <Button
+            colorScheme="blue"
+            mr={3}
+            onClick={() => {
+              randomData();
+            }}
+          >
+            Susprice
+          </Button>
+          <Button
+            colorScheme="blue"
+            onClick={() => {
+              nextData();
+            }}
+          >
+            Next
+          </Button>
+        </Flex>
+      </div>
+    </div>
+  );
+}
+
+export default App;
